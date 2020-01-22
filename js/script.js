c89e11c3ae3eebb994c4055334ec04cd6849fbf4
Jonathan Martinez <57384298+jonathan-martin-dev@users.noreply.github.com>
2020-01-21 22:54:37 -0800

    Add hide feature to colors field

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
+/* Helper Functions */
+//Set Attribute helper function
+const setAttr = (elem, attr, name) => {
+    elem.setAttribute(attr, name);
+}
+
 //First form field is focused on load
 const focusOnLoad = document.querySelector('#name');
 focusOnLoad.focus();
 
 //Add textfield if other option selected
 const jobRoleSelect = document.querySelector('#title');
-
-//Set Attribute helper function
-const setAttr = (elem, attr, name) => {
-    elem.setAttribute(attr, name);
-}
 
 //If JavaScript is turned on other option appears on DOM
 const otherOption = document.querySelector('.is-hidden');
@@ -26,4 +27,25 @@ jobRoleSelect.addEventListener('change', (e) => {
         fieldset.append(textArea);
     }
 });
-    
+
+//Set Color options to be hidden
+const colorSelect = document.querySelector('#color');
+const colors = colorSelect.children;
+
+colors[0].textContent = 'Please Select a T-Shirt';
+for (let i = 0; i < colors.length; i++) {
+    setAttr(colors[i], 'hidden', true);
+
+    const selectDesign = document.querySelector('#design');
+    selectDesign.addEventListener('change', (e) => {
+        const selectDesign = e.target.value;
+        if (selectDesign === 'js puns') {
+            if (colors[i].value === 'cornflowerblue' || colors[i].value === 'darkslategrey' || colors[i].value === 'gold') {
+               setAttr(colors[i], 'hidden', false);
+            }
+        }
+
+    });
+}
+
+
